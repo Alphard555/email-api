e1f40a17ff7a0d15ddd1bbbf77e77716b485f124
ОМ Савенков Алексей <13@ABIS.local>
2025-02-07 13:53:01 +1000

    Обновлен файл sendEmail.js с добавлением логирования

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,34 +1,22 @@
-// api/sendEmail.js
-
-const axios = require('axios');
-const aws4 = require('aws4');
-
-const AWS_KEY_ID = process.env.AWS_KEY_ID;  // Переменные окружения
-const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-const REGION = 'ru-central1';
-const SERVICE = 'ses';
-
 module.exports = async (req, res) => {
-  console.log('Запрос получен', req.method);
-
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Метод не поддерживается' });
   }
 
+  console.log('Запрос получен POST');
+  console.log('Получены данные для отправки:', req.body);
+
   const { toEmail, subject, body } = req.body;
 
-  console.log('Получены данные для отправки:', { toEmail, subject, body });
-
-  // Проверка обязательных полей
   if (!toEmail || !subject || !body) {
-    console.log('Ошибка: Не все обязательные поля предоставлены');
+    console.error('Ошибка: Не все обязательные поля предоставлены');
     return res.status(400).json({ error: 'Все поля обязательны' });
   }
 
   const data = {
     FromEmailAddress: 'no-reply@packmaster.tech',
     Destination: {
-      ToAddresses: [toEmail]  // Адрес получателя
+      ToAddresses: [toEmail]
     },
     Content: {
       Simple: {
@@ -46,11 +34,8 @@ module.exports = async (req, res) => {
     }
   };
 
-  console.log('Тело запроса, которое отправляем в Postbox:', data);
-
   const endpoint = 'https://postbox.cloud.yandex.net/v2/email/outbound-emails';
 
-  // Создаем запрос с AWS Signature V4
   const opts = {
     host: 'postbox.cloud.yandex.net',
     path: '/v2/email/outbound-emails',
@@ -61,13 +46,10 @@ module.exports = async (req, res) => {
     headers: { 'Content-Type': 'application/json' }
   };
 
-  // Подписываем запрос
   aws4.sign(opts, {
     accessKeyId: AWS_KEY_ID,
     secretAccessKey: AWS_SECRET_KEY,
   });
-
-  console.log('Подписанный запрос:', opts);
 
   try {
     const response = await axios({
@@ -77,10 +59,9 @@ module.exports = async (req, res) => {
       data: opts.body,
     });
 
-    console.log('Ответ от Postbox:', response.data);
     return res.status(200).json({ message: 'Письмо отправлено', data: response.data });
   } catch (error) {
-    console.error('Ошибка при отправке письма:', error);
+    console.error(error);
     return res.status(500).json({ error: 'Ошибка при отправке письма', details: error.message });
   }
 };
